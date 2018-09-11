a52fb2e13e251a734e02b1f11dffd8a33564e11b
congye6 <244053679@qq.com>
2018-09-11 16:34:19 +0800

    创建项目

---
+++ client/src/main/java/cn/edu/nju/congye6/netcloud/ClientApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju.congye6.netcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+}
