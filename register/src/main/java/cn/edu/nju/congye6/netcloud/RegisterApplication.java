b2a78f7e7fdee7519bc9033f25dc3b7494a937da
congye6 <244053679@qq.com>
2018-09-12 15:30:26 +0800

    创建项目

---
+++ register/src/main/java/cn/edu/nju/congye6/netcloud/RegisterApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju.congye6.netcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterApplication.class, args);
+	}
+}
---
+++ server/src/main/java/cn/edu/nju/congye6/netcloud/ServerApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju.congye6.netcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
