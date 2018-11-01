8e2403e17a28d643611ece284aa4139782356b75
congye6 <244053679@qq.com>
2018-11-01 10:18:55 +0800

    load balancer

--- client/src/main/java/cn/edu/nju/congye6/netcloud/service_router/load_balancer/RoundRobinLoadBalancer.java
+++ client/src/main/java/cn/edu/nju/congye6/netcloud/service_router/load_balancer/RoundRobinLoadBalancer.java
@@ -1,19 +1,38 @@
 package cn.edu.nju.congye6.netcloud.service_router.load_balancer;
 
+import cn.edu.nju.congye6.netcloud.util.RandomUtil;
+
 import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 轮询
+ * 加锁操作
  * Created by cong on 2018-10-31.
  */
 public class RoundRobinLoadBalancer implements LoadBalancer{
+
+    /**
+     * 保存上次分配了第几个地址
+     */
+    private Map<String,Integer> ADDRESS_INDEX_MAP=new HashMap<>();
+
     @Override
-    public String next(String serviceName, List<String> addressList) {
-        return null;
+    public synchronized String next(String serviceName, List<String> addressList) {
+        //存在并发操作，需要加锁
+        //没有耗时操作
+        Integer index=ADDRESS_INDEX_MAP.get(serviceName);
+        if(index==null)
+            index=-1;
+        index=(index+1)%addressList.size();
+        ADDRESS_INDEX_MAP.put(serviceName,index);
+        return addressList.get(index);
     }
 
     @Override
-    public void addressChangeNotice(String serviceName, List<String> addressList) {
-
+    public synchronized void addressChangeNotice(String serviceName, List<String> addressList) {
+        ADDRESS_INDEX_MAP.put(serviceName, RandomUtil.randomNumber(addressList.size()));
     }
 }
